9d24e9a8431e5eb0b7f3e796654469160bda2d45
firesworder <ivanviktor93@gmail.com>
2023-04-17 18:26:59 +0300

    iter12 Дополнил моки sql

--- internal/interfaces.go
+++ internal/interfaces.go
@@ -10,4 +10,19 @@ type DBStorage interface {
 	io.Closer
 	Query(query string, args ...any) (*sql.Rows, error)
 	Exec(query string, args ...any) (sql.Result, error)
+	Prepare(query string) (*sql.Stmt, error)
+}
+
+type DBStmt interface {
+	Query(args ...any) (*sql.Rows, error)
+	Exec(args ...any) (sql.Result, error)
+}
+
+type DBResult interface {
+	sql.Result
+}
+
+type DBRows interface {
+	Next() bool
+	Scan(args ...any) error
 }
