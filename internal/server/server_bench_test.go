99d814b2b88a79a348e8c135c3a34a3385cc0a65
Firesworder <ivanviktor93@gmail.com>
2023-06-25 19:11:50 +0300

    iter15 Реализовал бенчмарки для хэндлеров сервера(отдельно вариант для хэндлеров с БД и мемстореджем)

--- internal/server/server_bench_test.go
+++ internal/server/server_bench_test.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
 	"github.com/firesworder/devopsmetrics/internal"
 	"github.com/firesworder/devopsmetrics/internal/message"
@@ -26,16 +28,33 @@ func getMetricsMap() map[string]storage.Metric {
 	}
 }
 
-func getServer() (*Server, *storage.MemStorage) {
-	// подготовка сервера для примера
+func resetDBState(dbConn *sql.DB) {
+	ctx := context.Background()
+	// подготовка состояния таблицы
+	dbConn.ExecContext(ctx, "DELETE FROM metrics")
+	for _, metric := range getMetricsMap() {
+		mN, mV, mT := metric.GetMetricParamsString()
+		dbConn.ExecContext(ctx, "INSERT INTO metrics(m_name, m_value, m_type) VALUES($1, $2, $3)", mN, mV, mT)
+	}
+}
+
+// todo: реализовать две версии, одну для MemStorage, вторую для SQLStorage.
+func getServer(useDB bool) *Server {
+	if useDB {
+		Env.DatabaseDsn = "postgresql://postgres:admin@localhost:5432/devops"
+	} else {
+		Env.DatabaseDsn = ""
+	}
+
 	s, err := NewServer()
 	if err != nil {
 		panic(err)
 	}
 	s.LayoutsDir = "./html_layouts/"
-	nms := storage.NewMemStorage(getMetricsMap())
-	s.MetricStorage = nms
-	return s, nms
+	if !useDB {
+		s.MetricStorage = storage.NewMemStorage(getMetricsMap())
+	}
+	return s
 }
 
 func sendRequest(method, url, contentType, content string) (int, string, string) {
@@ -53,12 +72,12 @@ func sendRequest(method, url, contentType, content string) (int, string, string)
 	return resp.StatusCode, resp.Header.Get("Content-Type"), string(respBody)
 }
 
-func BenchmarkHandlers(b *testing.B) {
-	b.Run("getMetric", func(b *testing.B) {
-		s, _ := getServer()
-		ts := httptest.NewServer(s.Router)
-		defer ts.Close()
+func BenchmarkHandlersMemStorage(b *testing.B) {
+	s := getServer(false)
+	ts := httptest.NewServer(s.Router)
+	defer ts.Close()
 
+	b.Run("getMetric", func(b *testing.B) {
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
 			sendRequest(http.MethodGet, ts.URL+"/value/counter/PollCount", "text/plain", "")
@@ -66,53 +85,38 @@ func BenchmarkHandlers(b *testing.B) {
 	})
 
 	b.Run("showAllMetrics", func(b *testing.B) {
-		s, _ := getServer()
-		ts := httptest.NewServer(s.Router)
-		defer ts.Close()
-
-		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
 			sendRequest(http.MethodGet, ts.URL+"/", "text/plain", "")
 		}
 	})
 
 	b.Run("AddUpdateMetric", func(b *testing.B) {
-		s, nms := getServer()
-		ts := httptest.NewServer(s.Router)
-		defer ts.Close()
-
 		urlParams := `/update/counter/PollCount/20`
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			b.StopTimer()
-			nms.Metrics = getMetricsMap()
-			b.StartTimer()
 			sendRequest(http.MethodPost, ts.URL+urlParams, "text/plain", "")
+
+			b.StopTimer()
+			s.MetricStorage = storage.NewMemStorage(getMetricsMap())
+			b.StartTimer()
 		}
 	})
 
 	b.Run("JSONAddUpdateMetric", func(b *testing.B) {
-		s, nms := getServer()
-		ts := httptest.NewServer(s.Router)
-		defer ts.Close()
-
 		metric, _ := storage.NewMetric("CounterMetric1", internal.CounterTypeName, int64(15))
 		jsonMsg, _ := json.Marshal(metric.GetMessageMetric())
 		url := "/update/"
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			b.StopTimer()
-			nms.Metrics = getMetricsMap()
-			b.StartTimer()
 			sendRequest(http.MethodPost, ts.URL+url, "application/json", string(jsonMsg))
+
+			b.StopTimer()
+			s.MetricStorage = storage.NewMemStorage(getMetricsMap())
+			b.StartTimer()
 		}
 	})
 
 	b.Run("JSONGetMetric", func(b *testing.B) {
-		s, _ := getServer()
-		ts := httptest.NewServer(s.Router)
-		defer ts.Close()
-
 		jsonMsg, _ := json.Marshal(message.Metrics{ID: "PollCount"})
 		url := "/value/"
 		b.ResetTimer()
@@ -122,10 +126,6 @@ func BenchmarkHandlers(b *testing.B) {
 	})
 
 	b.Run("BatchUpdate", func(b *testing.B) {
-		s, nms := getServer()
-		ts := httptest.NewServer(s.Router)
-		defer ts.Close()
-
 		m1, _ := storage.NewMetric("PollCount", internal.CounterTypeName, int64(40))
 		m2, _ := storage.NewMetric("Alloc", internal.GaugeTypeName, 13.345)
 		m3, _ := storage.NewMetric("CounterMetric1", internal.CounterTypeName, int64(99))
@@ -137,10 +137,84 @@ func BenchmarkHandlers(b *testing.B) {
 		url := "/updates/"
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			b.StopTimer()
-			nms.Metrics = getMetricsMap()
-			b.StartTimer()
 			sendRequest(http.MethodPost, ts.URL+url, "application/json", string(jsonMsg))
+
+			b.StopTimer()
+			s.MetricStorage = storage.NewMemStorage(getMetricsMap())
+			b.StartTimer()
+		}
+	})
+}
+
+func BenchmarkHandlersSQLStorage(b *testing.B) {
+	s := getServer(true)
+	ts := httptest.NewServer(s.Router)
+	defer ts.Close()
+
+	b.Run("getMetric", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			sendRequest(http.MethodGet, ts.URL+"/value/counter/PollCount", "text/plain", "")
+		}
+	})
+
+	b.Run("showAllMetrics", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			sendRequest(http.MethodGet, ts.URL+"/", "text/plain", "")
+		}
+	})
+
+	b.Run("AddUpdateMetric", func(b *testing.B) {
+		urlParams := `/update/counter/PollCount/20`
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			sendRequest(http.MethodPost, ts.URL+urlParams, "text/plain", "")
+
+			b.StopTimer()
+			resetDBState(s.DBConn)
+			b.StartTimer()
+		}
+	})
+
+	b.Run("JSONAddUpdateMetric", func(b *testing.B) {
+		metric, _ := storage.NewMetric("CounterMetric1", internal.CounterTypeName, int64(15))
+		jsonMsg, _ := json.Marshal(metric.GetMessageMetric())
+		url := "/update/"
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			sendRequest(http.MethodPost, ts.URL+url, "application/json", string(jsonMsg))
+
+			b.StopTimer()
+			resetDBState(s.DBConn)
+			b.StartTimer()
+		}
+	})
+
+	b.Run("JSONGetMetric", func(b *testing.B) {
+		jsonMsg, _ := json.Marshal(message.Metrics{ID: "PollCount"})
+		url := "/value/"
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			sendRequest(http.MethodPost, ts.URL+url, "application/json", string(jsonMsg))
+		}
+	})
+
+	b.Run("BatchUpdate", func(b *testing.B) {
+		m1, _ := storage.NewMetric("PollCount", internal.CounterTypeName, int64(40))
+		m2, _ := storage.NewMetric("Alloc", internal.GaugeTypeName, 13.345)
+		m3, _ := storage.NewMetric("CounterMetric1", internal.CounterTypeName, int64(99))
+		msgSlice := []message.Metrics{
+			m1.GetMessageMetric(), m2.GetMessageMetric(), m3.GetMessageMetric(),
+		}
+		jsonMsg, _ := json.Marshal(msgSlice)
+
+		url := "/updates/"
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			sendRequest(http.MethodPost, ts.URL+url, "application/json", string(jsonMsg))
+
+			b.StopTimer()
+			resetDBState(s.DBConn)
+			b.StartTimer()
 		}
 	})
 }
